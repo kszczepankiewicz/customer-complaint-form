963bf4b4afaf7488a11c16ec2834070103840f4a
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-25 12:32:59 +0100

    feat: add preliminary validation

--- script.js
+++ script.js
@@ -7,34 +7,35 @@ const orderNoEl = $('order-no')
 const productCodeEl = $('product-code')
 const quantityEl = $('quantity')
 
-const complaintsGroupEl = document.querySelectorAll('#complaints-group input')
+const complaintsGroupEl = $('complaints-group')
+const complaintsGroupInputEls = document.querySelectorAll('#complaints-group input')
 const otherCompliantEl = $('other-complaint')
 const complaintDescriptionEl = $('complaint-description')
 
-const solutionsGroupEl = document.querySelectorAll('#solutions-group input')
+const solutionsGroupEl = $('solutions-group')
+const solutionsGroupInputEls = document.querySelectorAll('#solutions-group input')
 const otherSolutionEl = $('other-solution')
 const solutionDescriptionEl = $('solution-description')
 
 const validateForm = () => {
     const obj = {
         'full-name': fullNameEl.value !== '',
-        email: /^[^@]+@[\.]\..+$/.test(emailEl.value),
+        email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(emailEl.value),
         'order-no': /^2024\d+$/.test(orderNoEl.value),
         'product-code': /^[a-zA-Z]{2}\d\d-[a-zA-Z]\d\d\d-[a-zA-Z]{2}\d$/.test(productCodeEl.value),
         'quantity': Number(quantityEl.value) > 0 && quantityEl.value % 1 === 0,
-        'complaints-group': Array.from(complaintsGroupEl).some(input => input.checked === true),
+        'complaints-group': Array.from(complaintsGroupInputEls).some(input => input.checked === true),
         'complaint-description': otherCompliantEl.checked ? (complaintDescriptionEl.value.length
-            >= 20) : true,
-        'solutions-group': Array.from(solutionsGroupEl).some(input => input.checked === true),
+            >= 20) : complaintDescriptionEl.value === '',
+        'solutions-group': Array.from(solutionsGroupInputEls).some(input => input.checked === true),
         'solution-description': otherSolutionEl.checked ? (solutionDescriptionEl.value.length
-            >= 20) : true,
+            >= 20) : solutionDescriptionEl.value === '',
     }
     return obj;
 }
 
 const isValid = obj => Object.values(obj).every(inp => inp);
 
-// debugger;
 
 form.addEventListener('submit', (e) => {
     e.preventDefault;
@@ -45,5 +46,10 @@ fullNameEl.addEventListener('change', (e) => e.target.style.borderColor = valida
 emailEl.addEventListener('change', (e) => e.target.style.borderColor = validateForm()['email'] ? 'green' : 'red')
 orderNoEl.addEventListener('change', (e) => e.target.style.borderColor = validateForm()['order-no'] ? 'green' : 'red')
 productCodeEl.addEventListener('change', (e) => e.target.style.borderColor = validateForm()['production-code'] ? 'green' : 'red')
-quantityEl.addEventListener('change', (e) => e.target.style.borderColor = validateForm()['quantity'] ? 'green' : 'red')
+quantityEl.addEventListener('change', (e) => e.target.style.borderColor = validateForm()['quantity'] ? 'green' : 'red');
 
+complaintDescriptionEl.addEventListener('change', (e) => e.target.style.borderColor = validateForm()['complaint-description'] ? 'green' : 'red');
+solutionDescriptionEl.addEventListener('change', (e) => e.target.style.borderColor = validateForm()['solution-description'] ? 'green' : 'red');
+
+complaintsGroupEl.addEventListener('change', (e) => complaintsGroupEl.style.borderColor = validateForm()['complaints-group'] ? 'green' : 'red');
+solutionsGroupEl.addEventListener('change', (e) => solutionsGroupEl.style.borderColor = validateForm()['solutions-group'] ? 'green' : 'red'); 
