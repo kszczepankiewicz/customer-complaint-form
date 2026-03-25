2a935b29104f292f661332ae6137738095dd18b2
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-25 11:07:05 +0100

    feat: add function validateForm

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
-const $ = id => document.getElementById('form');
+const $ = id => document.getElementById(id);
 const formEl = $('form');
+
+const fullNameEl = $('full-name')
 const emailEl = $('email');
 const orderNoEl = $('order-no')
-const productCodeEl = $('productCode')
+const productCodeEl = $('product-code')
 const quantityEl = $('quantity')
+
 const complaintsGroupEl = document.querySelectorAll('#complaints-group input')
-const complaintsDescriptionEl = $('complaints-description')
+const otherCompliantEl = $('other-complaint')
+const complaintDescriptionEl = $('complaint-description')
+
 const solutionsGroupEl = document.querySelectorAll('#solutions-group input')
-const otherEl = $('other-complaint')
+const otherSolutionEl = $('other-solution')
+const solutionDescriptionEl = $('solution-description')
 
 const validateForm = () => {
     const obj = {
-        'full-name': form.value !== '',
+        'full-name': fullNameEl.value !== '',
         email: /^[^@]+@[\.]\..+$/.test(emailEl.value),
         'order-no': /^2024\d+$/.test(orderNoEl.value),
         'product-code': /^[a-zA-Z]{2}\d\d-[a-zA-Z]\d\d\d-[a-zA-Z]{2}\d$/.test(productCodeEl.value),
         'quantity': Number(quantityEl) > 0,
         'complaints-group': Array.from(complaintsGroupEl).some(input => input.checked === true),
-        'complaint-description': otherEl
-
+        'complaint-description': otherCompliantEl.checked ? (complaintDescriptionEl.value.length
+            >= 20) : true,
+        'solution-description': otherSolutionEl.checked ? (solutionDescriptionEl.value.length
+            >= 20) : true,
     }
+    debugger;
 
     return obj;
 
 }
 
-console.log('otherEl')
-
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault;
-
-// })
+form.addEventListener('submit', (e) => {
+    e.preventDefault;
+    validateForm();
+})
